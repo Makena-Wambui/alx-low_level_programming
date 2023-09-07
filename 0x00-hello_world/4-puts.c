40cb9fb5a8a16d8cbfb37a9e58e0a31cd43b45ad
Makena-Wambui <iammakena@gmail.com>
2023-09-07 14:45:54 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,7 +13,7 @@
 int main(void)
 {
 int puts(const char *str);
-char s[] = "Programming is like building a multilingual puzzle";
+char s[] = "\"Programming is like building a multilingual puzzle";
 
 puts(s);
 return (0);
