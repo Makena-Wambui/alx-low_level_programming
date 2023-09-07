eb1ed3a39ddbc7a32fb079ea5b1e8770e5dba04f
Makena-Wambui <iammakena@gmail.com>
2023-09-07 14:43:05 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:This function is the entry point of the program
+ * It prints a string on std output followed by a new line
+ *
+ * This function takes no arguments and returns an integer exit status
+ *
+ * Return: Always 0 (on success)
+ */
+
+int main(void)
+{
+int puts(const char *str);
+char s[] = "Programming is like building a multilingual puzzle";
+
+puts(s);
+return (0);
+}
