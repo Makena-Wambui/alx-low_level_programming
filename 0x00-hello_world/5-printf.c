8b6c85644428d22ac6136bb057b73c10c07f6dca
Makena-Wambui <iammakena@gmail.com>
2023-09-07 16:41:06 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *
+ *Description:This function is the entry point of the program
+ *It prints a string on std output followed by a new line
+ *
+ *This function takes no arguments and returns an integer exit status
+ *
+ *Return: Always 0 (on success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
