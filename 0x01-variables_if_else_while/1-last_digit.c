eb7c697363bd0681261228978afae1a45cf4c855
Makena-Wambui <iammakena@gmail.com>
2023-09-08 11:39:30 +0000

    Modulo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,19 +13,20 @@ int main(void)
 {
 int n;
 int last_num;
+
 last_num = n % 10;/*use modulo to extract last digit of n */
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (last_num > 5)
+	if (last_num > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 	}
-else if (last_num == 0)
+	else if (last_num == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, last_num);
 	}
-else if (last_num < 6 && last_num != 0)
+	else if (last_num < 6 && last_num != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
 	}
