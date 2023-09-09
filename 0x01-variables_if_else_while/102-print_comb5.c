a85d90076c5bad99f1648f4cf2748a1de8a14254
Makena-Wambui <iammakena@gmail.com>
2023-09-09 05:51:20 +0000

    2 digit 2 number combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of the program.
+ *
+ * Description: This function prints two digit two num combos.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num_1 = 0;
+int num_2 = 0;
+
+num_1 = 0;
+while (num_1 <= 99)
+{
+while (num_2 <= 99)
+	{
+	putchar(num_1 / 10 + '0');/*to extract tens digit of num_1 */
+	putchar(num_1 % 10 + '0');/*to extract ones digit of num_1 */
+	putchar(' ');
+	putchar(num_2 / 10 + '0');
+	putchar(num_2 % 10 + '0');
+
+	if (num_1 != 99 || num_2 != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	num_2++;
+	}
+num_1++;
+num_2 = num_1;
+}
+putchar('\n');
+return (0);
+}
