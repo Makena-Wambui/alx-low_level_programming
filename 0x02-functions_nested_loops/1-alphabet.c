a598d3c9ea06981b026ec72925ef9864540d379b
Makena-Wambui <iammakena@gmail.com>
2023-09-12 09:01:04 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - this function prints a message to std output.
+ *
+ * Description: This function prints 'a' to 'z' to stdout.
+ *
+ * Return: 0 (on success).
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
