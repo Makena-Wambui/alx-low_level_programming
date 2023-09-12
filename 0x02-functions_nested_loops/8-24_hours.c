482e17eb15d002ddac09bb49153776d8f67f44bb
Makena-Wambui <iammakena@gmail.com>
2023-09-12 22:06:25 +0000

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - this function prints time to stdout.
+ *
+ * Description: this function prints every minute of every
+ * hour of the day.
+ *
+ * Return: void. Has no return value.
+ */
+
+void jack_bauer(void)
+{
+int hour;
+int min;
+
+for (hour = 00; hour < 24; hour++)
+{
+	for (min = 00; min < 60; min++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min  % 10) + '0');
+		_putchar('\n');
+	}
+}
+	return;
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - this is the entry point of the program.
+ *
+ * Description: this function prints time to std out.
+ *
+ * Return: Always 0 on success.
+ */
+
+int main(void)
+{
+jack_bauer();
+return (0);
+}
