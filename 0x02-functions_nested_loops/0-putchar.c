6c42ab94e285d489a5075353c4b8393d6f0d9bfb
Makena-Wambui <iammakena@gmail.com>
2023-09-12 07:14:15 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - this is the entry point of the program.
+ *
+ * Description: This function prints _putchar on stdout.
+ *
+ * Return: 0 0n success.
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	return (0);
+}
+
