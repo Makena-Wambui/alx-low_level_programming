7744a0cb3b90b55e40042e881f429c4f2498021e
Makena-Wambui <iammakena@gmail.com>
2023-09-12 09:40:17 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - This is the entry point of the program.
+ *
+ * Description: this function prints 'a' to 'z' on std output.
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
