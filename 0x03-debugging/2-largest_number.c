a2014a678ef8a942bc8608f2913e559cd3492c60
Makena-Wambui <iammakena@gmail.com>
2023-09-13 09:06:45 +0000

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if ((a > b && b > c) || (a > b && c > b))
 	{
 	largest = a;
 	}
-	else if (b > a && a > c)
+	else if ((b > a && a > c) || (b > a && c > a))
 	{
 	largest = b;
 	}
-	else if (c > a && a > b)
+	else if ((c > a && a > b) || (c > a && b > a))
 	{
 	largest = c;
-	}
-	else if (a > c && c > b)
-	{
-	largest = a;
 	}
 	return (largest);
 }
