0cc6c4ac7a9c4b3cac4a91f9be093e4ba4af3396
Makena-Wambui <iammakena@gmail.com>
2023-09-13 07:45:22 +0000

    Modified my code to ensure my argument is 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
 *positive_or_negative - this function prints to stdout.
@@ -10,8 +8,6 @@
 */
 void positive_or_negative(int i)
 {
-srand(time(0));
-i = rand() - RAND_MAX / 2;
 if (i > 0)
 {
 printf("%d is positive\n", i);
