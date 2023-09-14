3d85b61975401b62997cb38862d4b91dc57d753c
Makena-Wambui <iammakena@gmail.com>
2023-09-14 15:07:27 +0000

    Printing line using underscore

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - prints a line to stdout.
+ * @n:number of times '_' is to be printed.
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int m;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m = 1; m <= n; m++)
+		{
+			_putchar('_');
+		}
+	
+	_putchar('\n');
+	}
+}
