4cadd15693fa7502edb4a825e5825f7add1630b5
Makena-Wambui <iammakena@gmail.com>
2023-09-15 18:24:13 +0000

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,32 +22,25 @@ return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
 
 void print_remaining_days(int month, int day, int year)
 {
-int daysofmonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int leap = leapyr(year);
-
-if (leap)
-{
-daysofmonth[2] = 29;
-}
-if ((month < 1 || month > 12 || day < 1 || day > daysofmonth[month]))
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
-}
-
-int dayofyr = 0
-for (int i = 1; i < month; i++)
-{
-dayofyr += daysofmonth[i];
-}
-
-dayofyr += day;
-
-if (leap && month > 2)
-{
-dayofyr++;
-}
-
-printf("Day of the year: %d\n", dayofyr);
-printf("Remaining days:%d\n", leap ? (366 - dayofyr) : (365 - dayofyr));
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		if (month > 2 && day >= 60)
+	{
+		day++;
+	}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	}
+		else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
+	}
 }
