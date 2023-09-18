ef3f080a12910626f8506d9b39df5c9fcca26081
Makena-Wambui <iammakena@gmail.com>
2023-09-18 09:46:13 +0000

    Printing a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
+
 
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
+
+void _puts(char *str);
+
+int _putchar(char c);
 
 
 
