7fcf6c38e3860fbc7796b168211c4cf34e9bb585
Makena-Wambui <iammakena@gmail.com>
2023-09-20 20:22:09 +0000

    Comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * character by character.
  * @s1: character array.
  * @s2: character array.
- * Return:0 if equal,-15 if s1 is less than s2, 15 if s1 is greater.
+ * Return:0 if equal,(-)if s1 is less than s2, (+)if s1 is greater.
  */
 
 int _strcmp(char *s1, char *s2)
@@ -14,9 +14,9 @@ int _strcmp(char *s1, char *s2)
 	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
 	{ /*compare the ascii values in each string at index a*/
 		if (s1[a] < s2[a])
-			return (-15);
-		else if (s1[a] > s2[a])
-			return (15);
+			return (s1[a] - s2[a]);
+		else if (s2[a] < s1[a])
+			return (s1[a] - s2[a]);
 	}
 	return (0);
 }
