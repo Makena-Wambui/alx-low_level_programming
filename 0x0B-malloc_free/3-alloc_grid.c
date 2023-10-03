20fb7e66dd0f6c194dde3c6bbb9dcdbefa80ec08
iammakena@gmail.com <root@MAKENA>
2023-10-03 12:46:49 +0300

    Pain in my butt task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,17 +10,26 @@
 int **alloc_grid(int width, int height)
 {
 	int **my2dgrid;
-	int a, q, r;
+	int a, d, q, r;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	my2dgrid = malloc(sizeof(int *) * width);
+	my2dgrid = malloc(sizeof(int *) * height);
 	if (my2dgrid == NULL)
 		return (NULL);
-	for (a = 0; a < width; a++)
+	for (a = 0; a < height; a++)
 	{
-		my2dgrid[a] = (int *)malloc(sizeof(int) * height);
+		my2dgrid[a] = malloc(sizeof(int) * width);
+		if (my2dgrid[a] == NULL)
+		{
+			for (d = 0; d < a; d++)
+			{
+				free(my2dgrid[d]);
+			}
+			free(my2dgrid);
+			return (NULL);
+		}
 	}
 	for (q = 0; q < width; q++)
 	{
