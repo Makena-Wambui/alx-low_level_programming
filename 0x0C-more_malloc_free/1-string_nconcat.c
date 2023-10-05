77001769cb784d9c33596b58945cdcace1b3ccdf
iammakena@gmail.com <root@MAKENA>
2023-10-05 23:56:38 +0300

    Concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - this function concatenates two strings.
+ * @s1: dest string.
+ * @s2: src string.
+ * @n: no of bytes of s2 to be appended..
+ * Return: pointerbto new string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *pntr;
+	unsigned int lens1, lens2 = 0;
+	unsigned int a, b = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)/*find string lengths*/
+		lens1++;
+	for (a = 0; s2[a] != '\0'; a++)
+		lens2++;
+	if (n >= lens2)/*allocate mem for s1 and n bytes of s2*/
+	{
+		pntr = malloc(sizeof(char)  * (lens1 + lens2 + 1));
+	}
+	else if (n < lens2)
+		pntr = malloc(sizeof(char) * (lens1 + n + 1));
+	if (pntr == NULL)
+		return (NULL);
+	for (a = 0; a < lens1; a++)/*copy s1 into pntr*/
+		pntr[a] = s1[a];
+	if (n >= lens2)
+	{
+		for (a = 0; a < (lens1 + lens2) && s2[b] != '\0'; a++)
+		{
+			pntr[lens1 + a] = s2[b];
+			b++;
+		}
+	}
+	else if (n < lens2)
+	{
+		for (a = 0; a < (lens1 + n) && s2[b] != '\0'; a++)
+		{
+			pntr[lens1 + a] = s2[b];
+			b++;
+		}
+	}
+	pntr[a] = '\0';/*allocate null byte*/
+	return (pntr);
+}
