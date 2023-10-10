17269887a9ad47a48a4fa3b182e48231140725e5
iammakena@gmail.com <root@MAKENA>
2023-10-10 12:51:35 +0300

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * and the name of the dog's owner.
  */
 
-struct dog
+typedef struct dog /*old data type*/
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t; /*new data type*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
