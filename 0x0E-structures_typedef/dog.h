85e818d50976015566969765b7396b9fbed0d3c5
iammakena@gmail.com <root@MAKENA>
2023-10-10 10:06:07 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - this structure is called dog.
+ * @name : name of the dog.
+ * @age : age of the dog.
+ * @owner : owner of the dog.
+ *
+ * Description: It contains the name and age of the dog
+ * and the name of the dog's owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* #ifndef _DOG_H_ */
