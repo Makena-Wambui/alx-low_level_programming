ca1dfb6d88ba017622f2dc5cad95b470f55a0145
iammakena@gmail.com <root@MAKENA>
2023-10-24 13:35:21 +0300

    Sum of all nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_listint - this function returns the sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the first node.
+ * Return: sum.
+ */
+
+int sum_listint(listint_t *head)
+{
+
+	listint_t *temp;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	while (temp != NULL)
+	{
+		sum = sum + (temp->n);
+		temp = temp->next;
+	}
+	return (sum);
+}
