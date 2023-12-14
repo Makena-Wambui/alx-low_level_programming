6bf9e0c5fa834274c0dc7153f425ec28e4cadbcf
iammakena@gmail.com <root@MAKENA>
2023-12-14 14:29:34 +0300

    Insertion at a specific position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - this function inserts
+ * a new node at a given position.
+ * @h: double pointer to head
+ * @idx: index specified
+ * @n: data in new node
+ * Return: newnode
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h;
+	dlistint_t *newnode;
+	unsigned int i = 1;
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		printf("Malloc failure.\n");
+		return (NULL);
+	}
+	newnode->n = n;
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+	}
+	else
+	{
+		while (i < idx)
+		{
+			temp = temp->next;
+			i++;
+		}
+		newnode->next = temp->next;
+		newnode->prev = temp;
+		temp->next->prev = newnode;
+		temp->next = newnode;
+	}
+	return (newnode);
+}
