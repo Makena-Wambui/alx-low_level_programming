b6b224def017c7ae2c8fe5c35dcb8404e19f1fc2
iammakena@gmail.com <root@MAKENA>
2023-12-14 12:15:11 +0300

    Fixed Betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,8 +1,13 @@
 #include "lists.h"
+/**
+ * sum_dlistint - this function calculates the sum of the data part in a list
+ * @head: pointer to head node
+ * Return: sum
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
-        int sum = 0;
+	int sum = 0;
 
 	if (head == NULL)
 	{
