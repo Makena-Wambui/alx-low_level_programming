6869cb9b37d40add6f0f3fdf633e0f2fb1e1cf7a
iammakena@gmail.com <root@MAKENA>
2023-12-16 14:56:04 +0300

    Using ctypes module in a python script to load a shared library and call C functions from that library

--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -55,3 +55,6 @@ Don’t forget to push your main.h file in your repository, containing at least 
 
 
 Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory.
+
+
+Create a dynamic library that contains C functions that can be called from Python.
---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * add - this function adds two integers together
+ * @a: a
+ * @b: b
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - this function subtracts an integer from another
+ * @a: a
+ * @b: b
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - this function returns the product of two integers
+ * @a: a
+ * @b: b
+ * Return: a * b
+ */
+int  mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div- this function divides two integers
+ * @a: a
+ * @b: b
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - this function calculates the modulo of two integers
+ * @a: a
+ * @b: b
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
