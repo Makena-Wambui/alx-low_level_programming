3123cc90eb46c354ea581b8a8c3742b5b4a4c5bc
iammakena@gmail.com <root@MAKENA>
2023-12-31 14:23:35 +0300

    Separate chaining with use of nodes to handle collisions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,6 +35,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new;
 
+	if (ht == NULL)
+	{
+		printf("Cannot insert into a NULL hash table.\n");
+		return (0);
+	}
+
 	new = node_creation(key, value);
 	index = key_index((unsigned char *) key, ht->size);
 	/* logic for empty string.*/
