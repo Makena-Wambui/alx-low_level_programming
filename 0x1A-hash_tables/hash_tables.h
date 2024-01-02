a8f04b014c68efa038947e41dfeec63a8396ac9b
iammakena@gmail.com <root@MAKENA>
2024-01-02 16:25:50 +0300

    Print a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -47,6 +47,8 @@ hash_node_t *node_creation(const char *key, const char *value);
 
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
+void hash_table_print(const hash_table_t *ht);
+
 
 
 
