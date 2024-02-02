3c643f9f2a86296eccfac5e9f0863d87db7f5460
iammakena@gmail.com <root@MAKENA>
2024-02-02 15:58:01 +0300

    How to print a hash table by iterating through the entire array.

---
+++ 0x1A-hash_tables/hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - this function deletes a hash table using free.
+ * @ht: a pointer to the hash table we are deleting.
+ * Return: void.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *ptr;
+	hash_table_t *tmp = ht;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			/* to mantain access to the next node.*/
+			ptr = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = ptr;
+		}
+
+		ht->array[i] = NULL;
+	}
+
+	free(tmp->array);
+	tmp->array = NULL;
+	free(tmp);
+	tmp = NULL;
+}
