c8e41f8d3a3a201b0e8be2e65e97ede15cd54e8c
iammakena@gmail.com <root@MAKENA>
2024-02-15 15:01:35 +0300

    How to create a hash table.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - this function creates a hash table
+ * structure.
+ * @size: size of the hash table array.
+ *
+ * Return: table which is a pointer to the newly created
+ * table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int a;
+
+	/* call malloc to allocate memory for the hash table structure.*/
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+
+	table->array = (hash_node_t **) malloc(table->size * sizeof(*(table->array)));
+	if (table->array == NULL)
+		return (NULL);
+
+	/**
+	 * lets loop throught the array
+	 * and initialize all buckets to NULL
+	 * when needed, memory will
+	 * be allocated for them.
+	 */
+	for (a = 0; a < table->size; a++)
+	{
+		table->array[a] = NULL;
+	}
+	return (table);
+}
