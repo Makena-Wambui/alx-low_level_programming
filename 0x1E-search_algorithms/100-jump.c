2761f660367cd4ee41d1a640ceb1126b57c69c15
Makena-Wambui <iammakena@gmail.com>
2024-07-16 15:51:44 +0300

    Jump Search Algorithm.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,64 +1,60 @@
 #include "search_algos.h"
 
 /**
- * min - This function returns the lesser integer.
- * It returns the minimum of two integers
- * It is used to handle cases where the
- * block size exceeds the array size.
+ * min - This function returns the minimum of two integers.
  *
  * @a: integer a
- * @b: integer b
+ * @b: integer b.
  *
  * Return: a or b.
  */
 
 size_t min(size_t a, size_t b)
 {
-	return ((a < b) ? a : b);
+	if (b > a)
+		return (a);
+	else
+		return (b);
 }
 
 /**
- * jump_search - finds an element in a sorted array
- * using the jump sort algorithm.
+ * jump_search - this function finds an element in a sorted
+ * array using the jump search algorithm.
  *
- * @array: a pointer to the first element of the array to search in.
- * @size: size of the array.
- * @value: target element.
+ * @array:  a pointer to the first element of the array to search in.
+ * @size: the number of elements in array
+ * @value: is the value to search for
  *
- * Return: -1 or the index of the target element.
+ * Your function must return the first index where value is located.
+ * Return: -1 or index where tartget element is located.
+ *
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i;
-	size_t block_size = sqrt(size);
-	size_t prev_index = 0;
+	/* Finding block size to be jumped */
+	size_t step = sqrt(size);
 
-	if (array == NULL)
-		return (-1);
+	/* Finding the block where element is */
+	/* present (if it is present) */
+	size_t prev = 0;
 
-	while (array[min(block_size, size) - 1] < value)
+	while (array[min(step, size) - 1] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n",
-			min(block_size, size) - 1, array[min(block_size, size) - 1]);
-		prev_index = block_size;
-		block_size += sqrt(size);
-
-		if (prev_index >= size)
-		{
+		prev = step;
+		step += sqrt(size);
+		if (prev >= size)
 			return (-1);
-		}
 	}
 
-	printf("Value found between indexes [%ld] and [%ld]\n",
-		prev_index, min(block_size, size) - 1);
-	for (i = prev_index; i < min(block_size, size); i++)
+	while (array[prev] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		{
-			return (i);
-		}
+		prev++;
+
+		if (prev == min(step, size))
+			return (-1);
 	}
+	if (array[prev] == value)
+		return (prev);
+
 	return (-1);
 }
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -8,3 +8,14 @@ What is Linear Search:
 This is the process of sequentially examining each element in the data set, one after the other, starting from the first element, and comparing it to the target element. If the current element matches exactly with the target element, then true or the index of the current element is returned. Else the search proceeds iterating through the next elements, untila match is found or  the end is reached;upon which No Match is Found is returned.
 
 
+JUMP SEARCH ALGORITHM:
+--------------------------
+For searching an element in a sorted array.
+Works by dividing the array into blocks of fixed size and performing a linear search within each block.
+These are the steps:
+1. Determine the block size to be jumped
+	The optimal size of a block to be jumped is determined by sqrt(n) where n is the size of the array.
+2. Jump ahead in blocks: Start at the first element. Jump ahead by block size until you find a block where the
+	target element is smaller than than the last element of the block.
+3. Linear search within the block: Once you find the block where the target element could be,
+	perform a linear search within that block.
