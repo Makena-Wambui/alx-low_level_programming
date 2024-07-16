cb11d9fd216c961272c2dcaad98201f392d20781
Makena-Wambui <iammakena@gmail.com>
2024-07-16 19:05:56 +0300

    Interpolation Search.

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,6 +18,9 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0, high = (size - 1);
 	size_t pos;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		if (low == high)
@@ -37,15 +40,11 @@ int interpolation_search(int *array, size_t size, int value)
 		if (pos >= size)
 		{
 			printf("Value checked array[%ld] is out of range\n", pos);
-			return -1;
+			return (-1);
 		}
-		
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
-
 		if (array[pos] == value)
-		{
 			return (pos);
-		}
 		if (array[pos] < value)
 			low = pos + 1;
 		else
