0aaf139493cd46cffaff943a54f18a00fb4510a7
Makena-Wambui <iammakena@gmail.com>
2024-07-17 12:45:08 +0300

    Perform a jump search on a sorted singly linked list.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,22 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int low, int high);
@@ -14,4 +30,8 @@ int exponential_search(int *array, size_t size, int value);
 int binarySearch(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
 int binary_with_recursion(int *array, size_t low, size_t high, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
 #endif
